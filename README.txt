25852f23bae02859b9da0d40f313ad9ec1561f82
Aakash Sethi <aakashsethi20@gmail.com>
2016-09-25 19:47:45 -0400

    Import in eclipse and run the class Queens

---
+++ README.txt
@@ -0,0 +1,21 @@
+ITI 1121 - A
+Professor - Marcel Turcotte
+
+Assignment #1
+
+Team members : AAKASH SETHI & PRINCEJEECT SINGH SANDHU
+Student nos.: AAKASH SETHI - 7951898
+			  PRINCEJEET SINGH SANDHU - 7736952
+
+Both of us are in section A
+
+Lab nos.: AAKASH SETHI - 2
+		  PRINCEJEET SINGH SANDHU - 5
+
+
+The assignment was about the N-queen problem. It involves finding the optimal solution by implementing the Genetic Algorithm. We made an array of Individuals called Population. Each individual contains an array showing the solution. The optimal solution is selected based on its fitness, which is the number of conflicts of the queens in each individual.
+The algorithm involves crossing over two individuals, mutating an indivdual, generation of individuals and finally printing the number of generations, the best solution in the population.
+
+Thank You
+Cheers
+Aakash Sethi & Princejeet Singh Sandhu
---
+++ StudentInfo.java
@@ -0,0 +1,28 @@
+/**
+ * Contains a method <code>void display()</code> that all the <code>main</code> methods
+ * call to display the student information. Fill the box with your personal
+ * information.
+ * 
+ * @author Marcel Turcotte (turcotte@eecs.uottawa.ca)
+ */
+
+public class StudentInfo {
+
+	/**
+	 * Displays the student information: student name, id, section, etc for each
+	 * member of the team.
+	 */
+
+	public static void display() {
+
+		System.out.println("************************************************************");
+		System.out.println("*Name: AAKASH SETHI and PRINCEJEET SINGH SANDHU            *");
+		System.out.println("*Student No.: 7951898 and 7736952                          *");
+		System.out.println("*Course: ITI 1121 A                                        *");
+		System.out.println("*Lab no.: 2 Monday 7-10 pm and Lab no.: 5 Wednesday 7-10 pm*");
+		System.out.println("************************************************************");
+		System.out.println();
+
+	}
+
+}
